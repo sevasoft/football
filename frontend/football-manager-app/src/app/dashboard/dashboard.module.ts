63f14dc7ae3c1f4883f5b642dc3da75c2d930464
moonwake769 <exhilaratedreverie288@protonmail.ch>
2023-04-20 15:35:35 +0200

    minor changes

--- frontend/football-manager-app/src/app/dashboard/dashboard.module.ts
+++ frontend/football-manager-app/src/app/dashboard/dashboard.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+<<<<<<< HEAD
 import { SharedModule } from '../shared/shared.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
@@ -20,5 +21,16 @@ import { PlayersModule } from './players/players.module';
   exports: [
     DashboardComponent
   ]
+=======
+import { PlayerComponent } from './player/player.component';
+import { TeamComponent } from './teams/team/team.component';
+import { SharedModule } from '../shared/shared.module';
+import { TeamsComponent } from './teams/teams.component';
+
+@NgModule({
+  declarations: [PlayerComponent, TeamComponent, TeamsComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [PlayerComponent, TeamComponent],
+>>>>>>> 713b737 (minor changes)
 })
-export class DashboardModule { }
+export class DashboardModule {}
---
+++ frontend/football-manager-app/src/app/dashboard/teams/teams.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fm-teams',
+  templateUrl: './teams.component.html',
+  styleUrls: ['./teams.component.css']
+})
+export class TeamsComponent {
+
+}
