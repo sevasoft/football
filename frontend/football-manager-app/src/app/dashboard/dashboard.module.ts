d93048c6d96af9969d7b53514742b54f69d5a20c
luc-ra <121627104+luc-ra@users.noreply.github.com>
2023-04-20 15:46:22 +0200

    resolved dashboard module merge conflict

--- frontend/football-manager-app/src/app/dashboard/dashboard.module.ts
+++ frontend/football-manager-app/src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
 import { SharedModule } from '../shared/shared.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { PlayersModule } from './players/players.module';
+import { TeamsModule } from './teams/teams.module';
 
 
 
@@ -16,21 +16,11 @@ import { PlayersModule } from './players/players.module';
     CommonModule,
     SharedModule,
     PlayersModule,
+    TeamsModule,
     DashboardRoutingModule
   ],
   exports: [
     DashboardComponent
   ]
-=======
-import { PlayerComponent } from './player/player.component';
-import { TeamComponent } from './teams/team/team.component';
-import { SharedModule } from '../shared/shared.module';
-import { TeamsComponent } from './teams/teams.component';
-
-@NgModule({
-  declarations: [PlayerComponent, TeamComponent, TeamsComponent],
-  imports: [CommonModule, SharedModule],
-  exports: [PlayerComponent, TeamComponent],
->>>>>>> 713b737 (minor changes)
 })
-export class DashboardModule {}
+export class DashboardModule { }
--- frontend/football-manager-app/src/app/dashboard/teams/teams.module.ts
+++ frontend/football-manager-app/src/app/dashboard/teams/teams.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TeamsComponent } from './teams.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { TeamComponent } from './team/team.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TeamsComponent,
+    TeamComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
+  ],
+  exports: [
+    TeamsComponent
   ]
 })
 export class TeamsModule { }
