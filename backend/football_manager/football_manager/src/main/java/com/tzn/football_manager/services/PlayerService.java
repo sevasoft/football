d5142e1d91d93e3f56f5b59ce64a55d490853b7b
Chardia1 <c.d.oudejans@gmail.com>
2023-04-20 14:42:36 +0200

    playerservice update player met annotation

--- backend/football_manager/football_manager/src/main/java/com/tzn/football_manager/services/PlayerService.java
+++ backend/football_manager/football_manager/src/main/java/com/tzn/football_manager/services/PlayerService.java
@@ -7,11 +7,13 @@ import com.tzn.football_manager.repos.TeamRepo;
 import org.hibernate.FetchNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class PlayerService {
     @Autowired
     PlayerRepo playerRepo;
