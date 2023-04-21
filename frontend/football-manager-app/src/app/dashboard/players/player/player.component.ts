1848e4ea135eb13a377b0b750eeb0b5368d88d61
moonwake769 <exhilaratedreverie288@protonmail.ch>
2023-04-21 09:44:16 +0200

    fix: commented code temporary

--- frontend/football-manager-app/src/app/dashboard/players/player/player.component.ts
+++ frontend/football-manager-app/src/app/dashboard/players/player/player.component.ts
@@ -4,8 +4,8 @@ import { Player } from '../../../shared/player';
 @Component({
   selector: 'fm-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.css']
+  styleUrls: ['./player.component.css'],
 })
 export class PlayerComponent {
-  @Input() player: Player;
+  // @Input() player: Player;
 }
