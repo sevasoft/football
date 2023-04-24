368e3af7bcb701f95984d2f5ec24282ac8b17c16
moonwake769 <exhilaratedreverie288@protonmail.ch>
2023-04-24 15:49:27 +0200

    feat: added player dynamic path routing

--- frontend/football-manager-app/src/app/app-routing.module.ts
+++ frontend/football-manager-app/src/app/app-routing.module.ts
@@ -1,31 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PlayerComponent } from './players/player/player.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
-  }, {
+    component: HomeComponent,
+  },
+  {
     path: 'matches',
     loadChildren: () =>
-      import('./matches/matches.module')
-        .then(m => m.MatchesModule)
-  }, {
+      import('./matches/matches.module').then((m) => m.MatchesModule),
+  },
+  {
     path: 'teams',
     loadChildren: () =>
-      import('./teams/teams.module')
-        .then(m => m.TeamsModule)
-  }, {
+      import('./teams/teams.module').then((m) => m.TeamsModule),
+  },
+  {
     path: 'players',
     loadChildren: () =>
-      import('./players/players.module')
-        .then(m => m.PlayersModule)
-  }
+      import('./players/players.module').then((m) => m.PlayersModule),
+  },
+  { path: 'players/:id', component: PlayerComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/football-manager-app/src/app/players/player/player.component.ts
+++ frontend/football-manager-app/src/app/players/player/player.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'fm-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.css']
+  styleUrls: ['./player.component.css'],
 })
-export class PlayerComponent {
+export class PlayerComponent implements OnInit {
+  id: string;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id')!;
+  }
 }
