d57388a17e71aac18c6e0445f4fe38cbc605cdcc
luc-ra <121627104+luc-ra@users.noreply.github.com>
2023-04-24 15:30:54 +0200

    fixed routing modules imports

--- frontend/football-manager-app/src/app/players/players.module.ts
+++ frontend/football-manager-app/src/app/players/players.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayersComponent } from './players.component';
 import { PlayersListComponent } from './players-list/players-list.component';
+import { PlayersRoutingModule } from './players-routing.module';
 
 @NgModule({
   declarations: [PlayersComponent, PlayersListComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    PlayersRoutingModule
+  ],
   exports: [PlayersComponent],
 })
-export class PlayersModule {}
+export class PlayersModule { }
