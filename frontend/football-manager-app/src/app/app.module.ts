29d3b66c22ed7e434470661cff7daca61cd5ae00
Rick <Rick_Veldkamp@live.nl>
2023-04-24 15:31:05 +0200

    in between safe state 1

--- frontend/football-manager-app/src/app/app.module.ts
+++ frontend/football-manager-app/src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatchesComponent } from './matches/matches.component';
-import { MatchComponent } from './matches/match/match.component';
 import { PlayersModule } from './players/players.module';
 import { MatchesModule } from './matches/matches.module';
 import { TeamsModule } from './teams/teams.module';
